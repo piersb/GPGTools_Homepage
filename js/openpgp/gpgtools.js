859c5fba895b331afb47acdb6f3da36774bc2569
Alexander Willner <alex@willner.ws>
2011-09-26 23:38:18 +0200

    (no) need to hard code OpenPGP public key [#83]

--- js/openpgp/gpgtools.js
+++ js/openpgp/gpgtools.js
@@ -1,8 +1,21 @@
             var keytype = 1;
             var pubkey  = "CACUaeg1vjWpg+H8ZmdeJYOiyrasNfJ8mY+TIVkcU/zvMc0cKMeluuxii99JWOEoqmWZUWMIfZmLTckfSMfTMO96fKGaqtKQwh1yDwtRI4G58ZTBGLV9Qp1AsJ/yM4WrZP29Tjzog70PwgqSnPb1iaTzcquMPdh47948IGFdfnvpAPx2K0VIqU1Ce25ZzaBsX6aflmya/TuRHjgZMixzD/LBhABiUHge3FjCkBmRtNF7GwO1u31vzUFSQHxrOQALYwwkTpwHCC3loXvmIZGKWsVlCVg00SUBCT4m7tA4q4VEMIW4o0npy6JkkI8Otg9Jr6gvyC8YXI8+Z3a3Obz3cElXAAURCACFg80OeVhuExuOvO/b/AYgvAjMipUkrona1wA1ibyUocVWOxIoEmKBiwwStdF2C03oxRQ2qjdAZOYvO42oK9w/LVY9Zb4kIr4vM4xeeKmNe1YvDDXn7JVjBDEhgDBp9CnfwKeow5zmrAemdgewtWkDuW7/igD4KHbqhQgK90fS4ZKeXr6ETJzT23bgTIdLv4qKOE10stUKl4M3h91dkV6bGnEqcNyLzzy4pFZDf5dqr+UrjvZVi9gnItYotaHCEYrF4nAjtzNK1uKS2ja6gmEOp7D/OJLbZ6XG8SnpbnVD1llX4SXGOaKDb6z0OwilH7wEM40fxcpYH1A9+ICfsLKC";
             var keyid   = "07eae49adbcbe671";
+
+            /*
+            var keyfile = document.getElementById("pgpkey").href;
+            var keyfile = "http://www.gpgtools.org/files/gpgtools.asc";
+            //issue: Origin null is not allowed by Access-Control-Allow-Origin.
+            //issue: Cross origin requests are only supported for HTTP
+            var key     = ReadFile(keyfile);
+            var keydata = OpenPGP_GetPublicInformation(key);
+            var pubkey  = keydata['mpi'];
+            var keyid   = keydata['id'];
+            */
+
             document.getElementById("feedbackbutton") ? document.getElementById("feedbackbutton").value="encrypt and send" : null;
             function encrypt() {
+
                 var text = document.feedback.message.value;
                 var text_enc = OpenPGP_Encrypt(keyid, keytype, pubkey, text);
                 document.feedback.message.value = '';
