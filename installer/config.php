b5242625c5126d97bd1f14553c55c3975689bb78
Steve Bell <steve@gpgtools.org>
2012-02-20 13:22:58 +0100

    fix for minor inconsistency
    * installer-page: GPGTools Preferences -> GPGPreferences

--- installer/config.php
+++ installer/config.php
@@ -58,7 +58,7 @@ $features = array(
     "$root/gpgmail/images/logo-35px.png" => array("Integrated <a href='$root/gpgmail/index.php'>GPGMail</a> (OS X 10.5 to 10.7, Universal)", 'GPGMail'),
     "$root/keychain/images/logo-35px.png" => array("Integrated <a href='$root/keychain/index.php'>GPG Keychain Access</a> (OS X 10.5 to 10.7, Universal)", 'GPG Keychain Access'),
     "$root/gpgservices/images/logo.png" => array("Integrated <a href='$root/gpgservices/index.php'>GPGServices</a> (OS X 10.6 to 10.7)", 'GPGServices'),
-    "$root/gpgpreferences/images/logo-35px.png" => array("Integrated <a href='$root/gpgpreferences/index.php'>GPGToolsPreferences</a> (OS X 10.6 to 10.7)", 'GPGPreferences'),
+    "$root/gpgpreferences/images/logo-35px.png" => array("Integrated <a href='$root/gpgpreferences/index.php'>GPGPreferences</a> (OS X 10.6 to 10.7)", 'GPGPreferences'),
     "$root/macgpg2/images/logo-35px.png" => array("Integrated <a href='$root/macgpg2/index.php'>MacGPG 2</a> (OS X 10.5 to 10.7, Intel)", 'gpg2, gpg-agent, pinentry-mac, macgpg2'),
     "$root/macgpg1/images/logo-35px.png" => array("Integrated <a href='$root/macgpg1/index.php'>MacGPG 1</a> (OS X 10.5 to 10.7, Universal)", 'gpg1, macgpg1'),
     "$root/images/enigmail.png" => array("Integrated <a href='http://enigmail.mozdev.org/'>Enigmail</a> (Thunderbird 3 to 8)", 'Mozilla Enigmail'),
