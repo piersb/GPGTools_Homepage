36748cdc5c55d312b4d740e0ed1ecff0d48bec59
Steve Bell <steve@gpgtools.org>
2012-03-22 12:50:25 +0100

    integrated old changelog

--- gpgmail/config.php
+++ gpgmail/config.php
@@ -12,6 +12,18 @@ $release['1.3.3'] = mktime(12, 0, 0, 03, 22, 2011);
 $release['1.3.2'] = mktime(12, 0, 0, 03, 02, 2011);
 $release['1.3.1'] = mktime(12, 0, 0, 11, 16, 2010);
 $release['1.3.0'] = mktime(12, 0, 0, 07, 27, 2010);
+$release['1.2.0v56'] = mktime(0, 0, 0, 12, 30, 2008);
+$release['1.1.2v46'] = mktime(0, 0, 0, 2, 5, 2006);
+$release['1.1.1v44'] = mktime(0, 0, 0, 7, 17, 2005);
+$release['1.1v42'] = mktime(0, 0, 0, 4, 29, 2005);
+$release['1.0.2v37'] = mktime(0, 0, 0, 5, 14, 2004);
+$release['1.0.1v33'] = mktime(0, 0, 0, 11, 17, 2003);
+$release['1.0v30'] = mktime(0, 0, 0, 10, 25, 2003);
+$release['0.5.4v22'] = mktime(0, 0, 0, 9, 10, 2002);
+$release['0.5.3v20'] = mktime(0, 0, 0, 6, 23, 2002);
+$release['0.5.2v13'] = mktime(0, 0, 0, 11, 22, 2001);
+$release['0.5.1v8'] = mktime(0, 0, 0, 9, 8, 2001);
+$release['0.5v4'] = mktime(0, 0, 0, 2, 7, 2001);
 $current=$release[$version];
 
 $theSite->assign('title', 'GPGMail');
@@ -212,8 +224,256 @@ $changelog = array(
         'sparkle_size' => '2451447',
 		'a' => 'Added Sparkle updates',
 		'b' => 'Changed web site, author, copyright, and e-mail address information to reflect the new project structure',
-        ))
+        )),
+	'1.2.0v56' => array(date('d. F Y', $release['1.2.0v56']), array(
+		'a' => 'Available for 10.5 Leopard only',
+		'b' => 'Now under BSD license (more liberal than before)',
+		'c' => 'Project now located on SourceForge.net',
+		'd' => 'Better compatibility when decrypting messages from old mailers (e.g. handles encrypted attachments)',
+		'e' => 'Added Swedish localization by Erik Hedlund',
+		'f' => 'Added new keyserver to list (SKS)',
+		'g' => 'For ASCII encrypted/signed messages, enforces use of quoted-printable - more robust - Thanks to Greg A. Woods',
+		'h' => 'Fixed support for <code>gpg2</code> (provided that you have a working agent - see FAQ)',
+		'i' => 'Fixed problem with <code>GPGOpenPGPExecutablePath</code> (no longer necessary in most cases - see documentation)',
+		'j' => 'Fixed bug when sending signed+encrypted (ASCII) message with untrusted recipient, and GPGMail set to trust no key: an exception was logged, instead of being displayed to user.',
+		'k' => 'Fixed bug: was still signing when automatic signing but no matching account',
+		'l' => 'Fixed bug with manual choice of encryption keys: message might be sent without encryption/signature',
+		'm' => 'Fixed bug: draft additional headers were not removed when message was delivered',
+		'n' => 'Fixed bug with manual choice of keys: choice was lost when displaying again key list',
+		'o' => 'Fixed bug with AppleScript-generated messages, and redisplayed PGP buttons',
+		'p' => 'Fixed dependency on <code>/usr/local/bin/gpg</code>',
+		'q' => 'Fixed positioning of compose accessory view',
+		'r' => 'Fixed bug with ’verbose’ option in gpg.conf file',
+		's' => 'Fixed toggle triangle state (showing/hiding the extended signature info)',
+		't' => 'Fixed case where public key choice was still automatic even after user chose keys manually',
+		'u' => 'Fixed initial automatic choice of key according to account',
+		'v' => 'Fixed initial sign/encrypt settings, when they should be set by default',
+		'w' => 'Fixed bug with disabled toolbar items on detached viewers',
+		'x' => 'Fixed UI-compatibility problems with MailTags',
+		'y' => 'Fixed bug with non-English locales',
+		'z' => 'Fixed bug with ’Reply to All’ button',
+		'aa' => 'No longer supports Tiger (10.4)',
+		'ab' => 'No longer supports Panther (10.3)'
+		)), 
+	
+	'1.1.2v46' => array(date('d. F Y', $release['1.1.2v46']), array(
+		'a' => 'Last version for Panther/10.3',
+		'b' => 'Panther: fixed crashes',
+		'c' => 'Tiger: built as Universal Binary',
+		'd' => 'Now needs <code>gpg</code> &gt;= 1.4.x',
+		'e' => 'Tiger: temporarily disabled display of PGP elements in composer window when window is created via AppleScript, to avoid unsolvable bug - you can still change PGP settings via menu Message/PGP.',
+		'f' => 'Mail no longer freezes when with using untrusted keys for encryption',
+		'g' => 'Now fills in automatically PGP Key Download search field as soon as a key is missing (except when already performing search/import)',
+		'h' => 'Fixed passphrase encryption when signature was toggled on',
+		'i' => 'Fixed crash after message delivery failure',
+		'j' => 'Dutch localization by Marc-Siebren Kwadijk',
+		'k' => 'Added hidden user default, <code>GPGOpenPGPExecutablePath</code>',
+		'l' => 'Fixed uncaught exceptions when <code>gpg</code> not configured correctly',
+		'm' => 'Better error message when error related to specific key',
+		'n' => 'Better key server-related error messages (not yet localized though)',
+		'o' => 'Now accepts SHA-256, SHA-384 and SHA-512 hash algorithms',
+		'p' => 'Fixed compatibility-problem with MailTags',
+		'q' => 'Fixed sorting of keys in key chooser dialog',
+		'r' => 'Fixed bug when "only if message is unread" prefs were followed (when checked) even when automatic decryption/authentication was unchecked',
+		's' => 'Fixed problem where encryption was not settable until signing had been set, in some cases.',
+		't' => 'No longer tries to decrypt text/plain attachments',
+		'u' => 'Tries to decrypt/verify application/octet-stream attachments',
+		'v' => 'Fixed alignments in new message window'
+		)), 
+	
+	'1.1.1v44' => array(date('d. F Y', $release['1.1.1v44']), array(
+		'a' => 'Changed keyboard shortcuts for sign/encrypt/decrypt/verify, using same as PGP, because our ones didn’t work on Tiger anylonger',
+		'b' => 'Tiger: Fixed compatibility problem with Enigmail encrypted messages that could not get decrypted',
+		'c' => 'Fixed unlocalized error message NO_VALID_PRIVATE_KEY',
+		'd' => 'Tiger: added separator line to compose window (thanks to Felix Schaumburg)',
+		'e' => 'Changed pref text: reply to <strong>PGP</strong> encrypted/signed',
+		'f' => 'Warning icon was no longer displayed when missing PGP keys',
+		'g' => 'Tiger: toolbar icons take less space now',
+		'h' => 'Tiger: Encryption/signing works again when performed from menu/toolbar buttons, without PGP buttons in composer',
+		'i' => 'Tiger: it was no longer possible to choose any PGP keys manually',
+		'j' => 'Spanish localization by Sergio Rodríguez Concha',
+		'k' => 'Finnish localization fix by Seppo Laaksonen',
+		'l' => 'Japanese localization by Tomio Arisaka',
+		'm' => 'Sender’s PGP information (sign/encrypt/MIME) stored in AddressBook is no longer evaluated by rules',
+		'n' => 'When evaluating for reply/forwarded, don’t use signed/encrypted information from S/MIME messages',
+		'o' => 'Fixed German localization',
+		'p' => 'Added hidden option, <code>GPGAddCustomHeaders</code>',
+		'q' => 'Tiger: Fixed install on 10.4.1 and later',
+		'r' => 'Fixed alignment of some items in composer window',
+		's' => 'Now links to MacGPGME dynamically, to avoid license problem',
+		't' => 'ASCII signed messages: fixed problem with Usenet signatures',
+		'u' => 'Tiger: fixed auto-decryption/auto-verification of unread messages',
+		'v' => 'Tiger: fixed refresh problem of compose accessory view when To/CC fields get higher',
+		'w' => 'Tiger: fixed iPhoto problem',
+		'x' => 'Added Danish localization by Sebastian Adorjàn Dyhr',
+		'y' => 'Fixed some ASCII signature verification problems',
+		'z' => 'Fixed UI bug when message is created by AppleScript',
+		'aa' => 'Tiger: Fixed assertion failure when verifying messages',
+		'ab' => 'Tiger: Was sometimes failing to report error'
+		)), 
+	
+	'1.1v42' => array(date('d. F Y', $release['1.1v42']), array(
+		'a' => 'Added support for Tiger (10.4)',
+		'b' => 'Fixed invalid display of keys being revoked/expired/disabled/invalid',
+		'c' => 'Fixed compatibility problem with Windows’ EudoraGPG, thanks to Georg Wedemeyer',
+		'd' => 'Fixed Finnish translation (courtesy of Asko Kauppi)',
+		'e' => 'Added basic support for AddressBook groups',
+		'f' => 'Added automatic settings (encryption, signature) based on current context',
+		'g' => 'Added retrieval of people <code>gpg</code> options from their Address Book card (set by Robert Goldsmith’s ABKey bundle)',
+		'h' => 'No longer tries to use invalid secret key when encrypting',
+		'i' => 'Got rid of GPGMEProxyServer',
+		'j' => 'Much faster PGP operations, notably key listing',
+		'k' => 'When a draft is restored, the encryption and signature states are restored too',
+		'l' => 'Fixed problem with attachments having non-ASCII filenames: now these chars are replaced by ’_’, but that doesn’t affect final filename, except maybe on old mail clients.',
+		'm' => 'Fixed bug with default private key which needed to be selected manually to get actually selected',
+		'n' => 'Lot of new options',
+		'o' => 'Added PGP-settings conflict resolution panel on delivery',
+		'p' => 'Now correctly recognizes revoked/disabled keys',
+		'q' => 'No longer reports wrongly a hash algo choice error instead of the unability to sign',
+		'r' => 'Only English, French, Finnish and German localizations are complete',
+		's' => 'And more... to be documented'
+		)), 
+	
+	'1.0.2v37?' => array(date('d. F Y', $release['1.0.2v37']), array(
+		 'a' => 'New icons by Michel Poulain (thanks also to Moritz Guth)',
+		 'b' => 'Corrected default key selection restoration',
+		 'c' => 'Corrected Spanish localization',
+		 'd' => 'Corrected scrolling of signature banner title',
+		 'e' => '10.2: no longer fails to load 10.3-compiled version, but catches error and reports it to user',
+		 'f' => 'New (hidden) option, <code>GPGRefreshesKeysOnVolumeMount</code>, to disable automatic refresh of keys when volume is (un)mounted; default value is YES.',
+		 'g' => 'New (hidden) option, <code>GPGDisablesSMIME</code>, to totally disable S/MIME encryption/signature. Default value is NO.',
+		 'h' => 'No longer considers text/plain attachments as potential PGP parts',
+		 'i' => 'Fixed incompatibility with PGP7/Outlook: for encrypted messages, always uses <code>content-transfer-encoding: 7bit</code>',
+		 'j' => 'When keychain passphrase was wrong, keychain item was not deleted',
+		 'k' => 'Corrected recognition of disabled/revoked secret/public keys',
+		 'l' => 'User could select revoked private key -&gt; signed message was empty',
+		 'm' => 'New feature: if user shows all keys (no filter), then display key status (revoked/disabled/expired)',
+		 'n' => 'New feature: extended key server list, like in GPGPreferences',
+		 'o' => 'New feature: show alert when signer’s key is revoked/disabled/...',
+		 'p' => 'Fixed: if you select passphrase encryption then turn it back off, the signing option is unticked (when it should be ticked).',
+		 'q' => 'New feature: after encryption failure due to missing keys, user can now select to send unencrypted',
+		 'r' => 'Fixed: 10.3: Account pulldown menu in composer window is randomly greyed. Happens as soon as changing account changes PGP key, from now on.',
+		 's' => '10.3: changed disclosure triangle in signature banner',
+		 't' => 'Compose window: warning icon is now displayed in popdown menu',
+		 'u' => 'Added hidden option <code>GPGUsesAlwaysCRLF</code> to always use CRLF as line-ending for PGP-inline encryption/signing; default value is NO.',
+		 'v' => 'Corrected recognition of disabled/revoked public keys in compose panel',
+		 'w' => 'Fixed enabling/disabling of toolbar/menu items when multiple windows are open',
+		 'x' => 'Fixed rare dead-lock at startup',
+		 'y' => 'Last release with 10.2-compatibility'
+		 )), 
+	
+	'1.0.1v33' => array(date('d. F Y', $release['1.0.1v33']), array(
+		'a' => 'When PGPmail is installed, you’re no longer forced to quit, but you’re warned that it’s at your own risks.',
+		'b' => 'No longer leaves orphan GPGMEProxyServer processes on quit or crash',
+		'c' => 'Corrected some error messages',
+		'd' => 'Key search results sometimes didn’t display correct names (problem with accents)',
+		'e' => 'Key search now displays "revoked" status in results',
+		'f' => 'Now refreshes keys each time a volume has been (un)mounted',
+		'g' => 'Added Finnish localization by Seppo Laaksonen',
+		'h' => 'Added Japanese localization by Kory Talmage',
+		'i' => 'Applied typographical rules to French localization',
+		'j' => 'Corrected some resizing errors',
+		'k' => 'Corrected installation problem with FileVault',
+		'l' => 'Corrected installation problem in <code>/Network/Users/</code>',
+		'm' => 'Corrected problem with some keys which have ISOLatin1 string encoding for the userIDs, when decrypting/signing',
+		'n' => 'Installer checks now system version',
+		'o' => 'No longer leaves a sticky "Done." message in the status view',
+		'p' => 'Added Korean localization by Sangduk Park',
+		'q' => 'Corrected problem with other mailers that added an empty line between each line when verifying/decrypting a PGP message',
+		'r' => 'Replying/forwarding/redirecting a decrypted message now quotes the decrypted message'
+		)), 
+	
+	'1.0v30' => array(date('d. F Y', $release['1.0v30']), array(
+		'a' => 'Completely reviewed code; now uses GPGME (indirectly)',
+		'b' => 'Replaced most of modal dialogs by non-modal ones, à la Junk Mail',
+		'c' => 'Added support for KeyChain, thanks to Annard Brouwer',
+		'd' => 'Added support for multiple secret keys',
+		'e' => 'User can now choose private/public keys; automatically, or manually',
+		'f' => 'Added symetric encryption support (encryption without key, only with shared passphrase)',
+		'g' => 'Preferences panel has been completely revamped, with new options',
+		'h' => 'New option: enable/disable trust of keys according to web-of-trust',
+		'i' => 'New option: choose OpenPGP-MIME format; automatically, or manually',
+		'j' => 'User can use BCC recipients too',
+		'k' => 'Decryption of encrypted and signed messages no longer fails when when signature cannot be checked (missing key, etc.)',
+		'l' => 'Corrected AppleScript incompatibility',
+		'm' => 'New option: sign when encrypting',
+		'n' => 'New option: decrypt/authenticate automatically only when message is unread',
+		'o' => 'New option: in passphrase panel, you can display typed passphrase',
+		'p' => 'When encrypting message, if you miss a key, GPGMail will try to find one on your default keyserver.',
+		'q' => 'Added ability to search for keys on keyservers.',
+		'r' => 'New option: line wrapping',
+		's' => 'New option: always encrypt',
+		't' => 'After computer sleep, passphrase timeout is now reset',
+		'u' => 'Corrected Installer problem with path containing aliases/links',
+		'v' => 'Corrected problem with format=flowed',
+		'w' => 'Now checks presence of PGPmail',
+		'x' => 'Hash algorithm is set according to your gpg preferences',
+		'y' => 'Dropped support for 10.1',
+		'z' => 'Added support for 10.3'
+		)), 
+	
+	'0.5.4v22' => array(date('d. F Y', $release['0.5.4v22']), array(
+		'a' => 'Added Swedish translation from Daniel Aarno',
+		'b' => 'Added Italian translation from Filippo Stefanelli',
+		'c' => 'Corrected some glitches with passphrase panel',
+		'd' => 'No longer leaves files in <code>/tmp</code>',
+		'e' => 'Added an installer',
+		'f' => 'Documentation translated in French',
+		'g' => 'License has been modified',
+		'h' => 'Corrected Korean translation',
+		'i' => 'Added support for MacOS X 10.2.x',
+		'j' => 'Corrected compatibility problem between contextual menu and other Mail plug-ins'
+		)), 
+	
+	'0.5.3v20' => array(date('d. F Y', $release['0.5.3v20']), array(
+		'a' => 'Added Spanish translation from Sergio Rodríguez Concha',
+		'b' => 'Added French translation from Michel Poulain',
+		'c' => 'Added Japanese translation from Tomio Arisaka',
+		'd' => 'Added beautiful icons from Michel Poulain',
+		'e' => 'Modified "decrypted" icon, by Jamin Wilson',
+		'f' => 'Added new userDefaults, <code>GPGOpenPGPStyleOnly</code> (default value is NO), to force always using OpenPGP-style encryption/signing',
+		'g' => 'Corrected problems with attachments which were not yet loaded',
+		'h' => 'Corrected problem with Kotoeri input method',
+		'i' => 'Added German translation from Hauke Müller',
+		'j' => 'Added Danish translation from Thomas Dyhr',
+		'k' => 'Stopped support for MacOS X Server (Rhapsody)',
+		'l' => 'Added Korean translation from Sangduk Park',
+		'm' => 'Added support for encapsulated signature (RFC1847); this is now the default method used when encrypting a signed message with OpenPGP style.',
+		'n' => 'Corrected state of newly added toolbar buttons',
+		'o' => 'Corrected problems due to character sets (no longer forces use of UTF8)',
+		'p' => 'Added contextual menu for decryption/authentication',
+		'q' => 'Corrected header decoding bug',
+		'r' => 'Corrected signature bug',
+		's' => 'Corrected compatibility problems with Sylpheed, Evolution, Entourage, Outlook, Eudora, etc.',
+		't' => 'Corrected compatibility problem with MacOS X 10.1.5',
+		'u' => 'Added basic support for HTML signed/encrypted messages',
+		'v' => 'Corrected problem with gpg 1.0.7: you no longer need to sign keys before using them for encryption. With gpg 1.0.6, you still need to: if you don’t, encryption is refused.',
+		'w' => 'Corrected problems with MailPriority &gt;= 1.1'
+		)), 
+	
+	'0.5.2v13' => array(date('d. F Y', $release['0.5.2v13']), array(
+		'a' => 'Ported to MacOS X 10.1.x',
+		'b' => 'Corrected problems with character sets',
+		'c' => 'Added keyboard shortcuts',
+		'd' => 'Now needs gpg version &gt;= 1.0.6',
+		'e' => 'No version for MacOS X Server 1.x'
+		)), 
+	
+	'0.5.1v8' => array(date('d. F Y', $release['0.5.1v8']), array(
+		'a' => 'Ported to MacOS X 10.0.4',
+		'b' => 'Corrected bugs in the application of RFC 3156',
+		'c' => 'Corrected lots of other bugs'
+		)), 
+	
+	'0.5v4' => array(date('d. F Y', $release['0.5v4']), array(
+		'a' => 'initial release, for MacOS X Server 1.x and MacOS X Public Beta'
+				 )) 
+              
 
+	
+	
+	
+	
 );
 $theSite->assign('changelog', $changelog);
 $theSite->assign('changelogfile', 'changelog.php');
