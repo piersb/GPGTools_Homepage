5d7819321b5a5d0bfc33829e85de2edccaa8739d
Mento <Mento@gpgtools.org>
2012-10-06 11:01:02 +0200

    testJSON.sh added.

---
+++ testJSON.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function echoError() {
+	if [[ -t 1 ]] ;then
+		echo -e "\033[1;31m$*\033[0m" >&2
+	else
+		echo "$*" >&2
+	fi
+}
+
+
+pushd "${0%/*}/config" >/dev/null || exit 1
+
+for file in *.json ;do
+	if ! error=$(python -mjson.tool <"$file" 2>&1 >/dev/null) ;then
+		echoError "$file: $error"
+	fi
+done
+
+popd >/dev/null
+
+
+
