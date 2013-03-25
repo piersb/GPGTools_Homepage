8bcc45e43797f3a757d8a8a22a29f23c27735ea7
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-03-25 01:41:04 +0100

    Allow either buildnr or version for updateVersion and appVersion

--- js/sparkle-release-notes.js
+++ js/sparkle-release-notes.js
@@ -1,9 +1,18 @@
 $(function() {
-    var updateVersionIdx = $('[data-version="' + paramFromQuery("updateVersion") + '"]').index()
-    var currentVersionIdx = $('[data-version="' + paramFromQuery("appVersion") + '"]').index()
-    var osVersion = paramFromQuery("osVersion")
+    var updateVersion = paramFromQuery("updateVersion")
+    var appVersion = paramFromQuery("appVersion")
     
-    console.log("osVersion: ", osVersion)
+    // version can be either x.x.x or only the build number.
+    var updateCheckAttribute = "version"
+    if(updateVersion.search(/\./) == -1)
+        updateCheckAttribute = "build"
+    var appCheckAttribute = "version"
+    if(appVersion.search(/\./) == -1)
+        appCheckAttribute = "build"
+    
+    var updateVersionIdx = $('[data-' + updateCheckAttribute + '="' + updateVersion + '"]').index()
+    var currentVersionIdx = $('[data-' + appCheckAttribute + '="' + appVersion + '"]').index()
+    var osVersion = paramFromQuery("osVersion")
     
     if(updateVersionIdx == -1 && currentVersionIdx == -1)
         return
@@ -15,15 +24,16 @@ $(function() {
         
         var versionSatisfiesRequirements = satisfiesMinVersion && satisfiesMaxVersion
         
-        console.log("satisfiesMinVersion: ", satisfiesMinVersion, " compare-versions: ", compareVersions(osVersion, $(this).data("min-os")))
-        
         if($(this).index() >= updateVersionIdx && $(this).index() < currentVersionIdx && versionSatisfiesRequirements)
             $(this).show()
     })
     
     /* If no item is visible, display the first one at least. */
-    if($(".version:visible").size() == 0)
-        $(".version:first").show()
+    if($(".version:visible").size() == 0) {
+        var $newestVersion = newestVersionForOS($(".version"), osVersion)
+        if($newestVersion !== null)
+            $newestVersion.show()
+    }
 })
 
 var URL_QUERY_STRING = null
@@ -52,4 +62,17 @@ function compareVersions(a, b) {
             return res >= 1 ? 1 : -1
     }
     return 0
+}
+
+function newestVersionForOS($versions, osVersion) {
+    $versions.each(function() {
+        var satisfiesMinVersion = !$(this).data("min-os") ? 1 : compareVersions(osVersion, $(this).data("min-os")) >= 0
+        var satisfiesMaxVersion = !$(this).data("max-os") ? 1 : compareVersions(osVersion, $(this).data("max-os")) <= 0
+        var versionSatisfiesRequirements = satisfiesMinVersion && satisfiesMaxVersion
+        
+        if(versionSatisfiesRequirements)
+            return $(this)
+    })
+    
+    return null
 }
