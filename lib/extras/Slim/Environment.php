a25a5ff1a811280a51fab2c980907efb303f77f3
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-03-29 02:29:50 +0100

    Correctly deal with cases where SCRIPT_NAME is not part of REQUEST_URI

--- lib/extras/Slim/Environment.php
+++ lib/extras/Slim/Environment.php
@@ -145,7 +145,14 @@ class Environment implements \ArrayAccess, \IteratorAggregate
             } else {
                 $env['SCRIPT_NAME'] = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']) ); //With URL rewrite
             }
-            $env['PATH_INFO'] = substr_replace($_SERVER['REQUEST_URI'], '', 0, strlen($env['SCRIPT_NAME']));
+            /* This fails for some reason if SCRIPT_NAME is not part of REQUEST_URI,
+             * which is the case on gpgtools rewrites config.
+             */
+            if(strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) !== false) 
+                $env['PATH_INFO'] = substr_replace($_SERVER['REQUEST_URI'], '', 0, strlen($env['SCRIPT_NAME']));
+            else
+                $env['PATH_INFO'] = $_SERVER['REQUEST_URI'];
+            
             if (strpos($env['PATH_INFO'], '?') !== false) {
                 $env['PATH_INFO'] = substr_replace($env['PATH_INFO'], '', strpos($env['PATH_INFO'], '?')); //query string is not removed automatically
             }
